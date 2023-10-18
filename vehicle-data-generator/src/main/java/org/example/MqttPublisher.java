9bdd365d9b3c3abc5f74b7dd24f20a910bbec08c
soldee <71665509+soldee@users.noreply.github.com>
2023-10-18 23:07:39 +0200

    kconnect init container that uploads the mongodb-sink json config file to kconnect

--- vehicle-data-generator/src/main/java/org/example/MqttPublisher.java
+++ vehicle-data-generator/src/main/java/org/example/MqttPublisher.java
@@ -6,6 +6,8 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
+import java.util.Random;
+
 public class MqttPublisher {
 
     public static void main(String[] args) {
@@ -34,10 +36,21 @@ public class MqttPublisher {
             throw new RuntimeException(e);
         }
 
-        for (int i=0; i<1000; i++) {
+        for (int i=10; i<1000; i++) {
             try {
-                client.publish("test-topic", ("message" + i).getBytes(), QOS, true);
-                Thread.sleep(3000);
+                String message = "{" +
+                        "\"ts\": \"2023-01-01T10:15:" + i + "Z\"," +
+                        "\"meta\":{" +
+                        "\"vehicle_id\":\"a\"," +
+                        "\"user_id\":\"a\"," +
+                        "\"route_id\":\"a\"" +
+                        "}," +
+                        "\"location\":{\"type\":\"Point\",\"coordinates\":[" + new Random().nextInt(10) + "," + new Random().nextInt(10) + "]}," +
+                        "\"speed\":" + new Random().nextFloat() * 10 +
+                        "}";
+
+                client.publish("test-topic", message.getBytes(), QOS, true);
+                Thread.sleep(10000);
             } catch (MqttException e) {
                 throw new RuntimeException(e);
             } catch (InterruptedException e) {
