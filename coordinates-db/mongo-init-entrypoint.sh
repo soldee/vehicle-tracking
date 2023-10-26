3fd6486722b40be448415cea82f01c1abc1caf0b
soldee <71665509+soldee@users.noreply.github.com>
2023-10-26 18:16:41 +0200

    mongo-init container to provision mongoDB changed + automate index creation on docker compose start

---
+++ coordinates-db/mongo-init-entrypoint.sh
@@ -0,0 +1,18 @@
+mongosh --host mongo:27017 -u root -p root --eval "
+	rs.initiate({
+		_id: 'rs0',
+		members: [
+			{_id:0, host: 'mongo'},
+		]});
+          
+        while (rs.status().ok == 0) sleep(1000);
+"
+
+sleep 3
+
+mongosh --host mongo:27017 -u root -p root --eval "
+	db = db.getSiblingDB('VEHICLE-TRACKING')
+	db.getCollection('vehicle-status').createIndex({'meta.vehicle_id':1, 'ts':-1});
+	db.getCollection('vehicle-status').createIndex({'meta.route_id':1});
+	db.getCollection('vehicle-status').createIndex({'meta.user_id':1, 'ts':-1});
+"
