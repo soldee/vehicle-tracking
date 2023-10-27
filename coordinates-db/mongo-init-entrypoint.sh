ed40e09c63a9efba4c9d02d4a9d349cba33320fe
soldee <71665509+soldee@users.noreply.github.com>
2023-10-27 18:20:44 +0200

    timeseries collection creation added to mongo-init container

--- coordinates-db/mongo-init-entrypoint.sh
+++ coordinates-db/mongo-init-entrypoint.sh
@@ -12,6 +12,22 @@ sleep 3
 
 mongosh --host mongo:27017 -u root -p root --eval "
 	db = db.getSiblingDB('VEHICLE-TRACKING')
+	db.createCollection(
+		'vehicle-status',
+		{
+		   timeseries: {
+			  timeField: 'ts',
+			  metaField: 'meta',
+			  granularity: 'seconds'
+		   }
+		}
+	)
+"
+	
+sleep 1
+
+mongosh --host mongo:27017 -u root -p root --eval "
+	db = db.getSiblingDB('VEHICLE-TRACKING')
 	db.getCollection('vehicle-status').createIndex({'meta.vehicle_id':1, 'ts':-1});
 	db.getCollection('vehicle-status').createIndex({'meta.route_id':1});
 	db.getCollection('vehicle-status').createIndex({'meta.user_id':1, 'ts':-1});
