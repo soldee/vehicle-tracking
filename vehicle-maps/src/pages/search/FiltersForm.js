1d974114a05d7e779df0c99d45a724d715ad9f98
soldee <71665509+soldee@users.noreply.github.com>
2023-11-09 13:32:13 +0100

    filters form layout and button loading animation

--- vehicle-maps/src/pages/search/FiltersForm.js
+++ vehicle-maps/src/pages/search/FiltersForm.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import './FiltersForm.css'
+
 
 export default function FiltersForm({ onSearchRouteId }) {
 
+    const [isLoading, setIsLoading] = useState(false)
+
+
     function handleSubmit(e) {
         e.preventDefault()
+        setIsLoading(true)
 
         const form = new FormData(e.target)
         const route_id = form.get("route_id_input")
+        const vehicle_id = form.get("vehicle_id_input")
+        const user_id = form.get("user_id_input")
 
         fetch(window.REACT_APP_DOMAIN + "/vehicle/status?" + new URLSearchParams({
             route_id: route_id
         }), { method: form.method })
             .then(async (response) => {
                 const json = await response.json();
+                setIsLoading(false)
 
                 if (!response.ok) {
                     const err = json.error
@@ -28,12 +37,48 @@ export default function FiltersForm({ onSearchRouteId }) {
     }
 
     return (
-        <form method="GET" onSubmit={handleSubmit}>
-            <label>
-                Route ID:
-                <input name="route_id_input" />
-            </label>
-            <button type="submit">Search</button>
+        <form method="GET" onSubmit={handleSubmit} className="filters-form">
+            <div className="filters">
+                <div className="metadata-filters">
+                    <h3>Metadata</h3>
+                    <div className="m-filter">
+                        <label>
+                            Route ID
+                            <input name="route_id_input" />
+                        </label>
+                    </div>
+                    <div className="m-filter">
+                        <label>
+                            Vehicle ID
+                            <input name="vehicle_id_input" />
+                        </label>
+                    </div>
+                    <div className="m-filter">
+                        <label>
+                            User ID
+                            <input name="user_id_input" />
+                        </label>
+                    </div>
+                </div>
+                <div className="date-filters">
+                    <h3>Dates</h3>
+                    <div className="m-filter">
+                        <label>
+                            From
+                            <input name="date_from_input" />
+                        </label>
+                    </div>
+                    <div className="m-filter">
+                        <label>
+                            To
+                            <input name="date_to_input" />
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <button type="submit" class={`submit-button ${isLoading && "button--loading"}`} >
+                <span class="button__text">Save</span>
+            </button>
         </form>
     )
 }
