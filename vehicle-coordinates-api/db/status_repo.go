e82a1db542f0d128a08d19b5333583d4ce9d9f5f
soldee <71665509+soldee@users.noreply.github.com>
2023-12-22 21:51:00 +0100

    add support for vehicle_id queries

--- vehicle-coordinates-api/db/status_repo.go
+++ vehicle-coordinates-api/db/status_repo.go
@@ -16,7 +16,12 @@ type StatusRepo interface {
 	FindByRouteIdBetween(ctx context.Context, RouteId string, DateFrom time.Time, DateTo time.Time) (*models.Status, error)
 	FindByUserId(ctx context.Context, UserId string) ([]models.Status, error)
 	FindByUserIdBetween(ctx context.Context, UserId string, DateFrom time.Time, DateTo time.Time) ([]models.Status, error)
+	FindByVehicleId(ctx context.Context, VehicleId string) ([]models.Status, error)
+	FindByVehicleIdBetween(ctx context.Context, VehicleId string, DateFrom time.Time, DateTo time.Time) ([]models.Status, error)
 	FindByRouteIdAndUserIdBetween(ctx context.Context, RouteId string, UserId string, DateFrom time.Time, DateTo time.Time) (*models.Status, error)
+	FindByRouteIdAndVehicleIdBetween(ctx context.Context, RouteId string, VehicleId string, DateFrom time.Time, DateTo time.Time) (*models.Status, error)
+	FindByUserIdAndVehicleIdBetween(ctx context.Context, UserId string, VehicleId string, DateFrom time.Time, DateTo time.Time) ([]models.Status, error)
+	FindByRouteIdAndUserIdAndVehicleIdBetween(ctx context.Context, RouteId string, UserId string, VehicleId string, DateFrom time.Time, DateTo time.Time) (*models.Status, error)
 }
 
 type MongoStatusRepo struct {
@@ -152,6 +157,78 @@ func (repo *MongoStatusRepo) FindByUserIdBetween(ctx context.Context, UserId str
 	return results, nil
 }
 
+func (repo *MongoStatusRepo) FindByVehicleId(ctx context.Context, VehicleId string) ([]models.Status, error) {
+	cursor, err := repo.coordinatesCollection.Aggregate(ctx, mongo.Pipeline{
+		{{Key: "$match", Value: bson.D{
+			{Key: "meta.vehicle_id", Value: VehicleId},
+		}}},
+		{{Key: "$group", Value: bson.D{
+			{Key: "_id", Value: "null"},
+			{Key: "ts", Value: bson.D{{Key: "$push", Value: "$ts"}}},
+			{Key: "coordinates", Value: bson.D{{Key: "$push", Value: "$location.coordinates"}}},
+			{Key: "speed", Value: bson.D{{Key: "$push", Value: bson.D{{Key: "$trunc", Value: bson.A{"$speed", 2}}}}}},
+			{Key: "route_id", Value: bson.D{{Key: "$first", Value: "$meta.route_id"}}},
+			{Key: "vehicle_id", Value: bson.D{{Key: "$first", Value: "$meta.vehicle_id"}}},
+			{Key: "user_id", Value: bson.D{{Key: "$first", Value: "$meta.user_id"}}},
+		}}},
+		{{Key: "$project", Value: bson.D{
+			{Key: "_id", Value: 0},
+		}}},
+	})
+	if err != nil {
+		return nil, &response.InternalError{Msg: "FindByVehicleId aggregation generated error:" + err.Error()}
+	}
+
+	defer cursor.Close(ctx)
+
+	var results []models.Status
+	if err = cursor.All(ctx, &results); err != nil {
+		return nil, &response.InternalError{Msg: "FindByVehicleId error unmarshalling bson response into Status model: " + err.Error()}
+	}
+
+	if len(results) == 0 {
+		return nil, &response.NotFound{Msg: fmt.Sprintf("vehicle_id %v not found", VehicleId)}
+	}
+
+	return results, nil
+}
+
+func (repo *MongoStatusRepo) FindByVehicleIdBetween(ctx context.Context, VehicleId string, DateFrom time.Time, DateTo time.Time) ([]models.Status, error) {
+	cursor, err := repo.coordinatesCollection.Aggregate(ctx, mongo.Pipeline{
+		{{Key: "$match", Value: bson.D{
+			{Key: "meta.vehicle_id", Value: VehicleId},
+			{Key: "ts", Value: bson.D{{Key: "$gt", Value: DateFrom}, {Key: "$lt", Value: DateTo}}},
+		}}},
+		{{Key: "$group", Value: bson.D{
+			{Key: "_id", Value: "null"},
+			{Key: "ts", Value: bson.D{{Key: "$push", Value: "$ts"}}},
+			{Key: "coordinates", Value: bson.D{{Key: "$push", Value: "$location.coordinates"}}},
+			{Key: "speed", Value: bson.D{{Key: "$push", Value: bson.D{{Key: "$trunc", Value: bson.A{"$speed", 2}}}}}},
+			{Key: "route_id", Value: bson.D{{Key: "$first", Value: "$meta.route_id"}}},
+			{Key: "vehicle_id", Value: bson.D{{Key: "$first", Value: "$meta.vehicle_id"}}},
+			{Key: "user_id", Value: bson.D{{Key: "$first", Value: "$meta.user_id"}}},
+		}}},
+		{{Key: "$project", Value: bson.D{
+			{Key: "_id", Value: 0},
+		}}},
+	})
+	if err != nil {
+		return nil, &response.InternalError{Msg: "FindByVehicleIdBetween aggregation generated error:" + err.Error()}
+	}
+
+	defer cursor.Close(ctx)
+
+	var results []models.Status
+	if err = cursor.All(ctx, &results); err != nil {
+		return nil, &response.InternalError{Msg: "FindByVehicleIdBetween error unmarshalling bson response into Status model: " + err.Error()}
+	}
+
+	if len(results) == 0 {
+		return nil, &response.NotFound{Msg: fmt.Sprintf("No routes found for vehicle_id %v between the specified dates %v-%v", VehicleId, DateFrom, DateTo)}
+	}
+	return results, nil
+}
+
 func (repo *MongoStatusRepo) FindByRouteIdAndUserIdBetween(ctx context.Context, RouteId string, UserId string, DateFrom time.Time, DateTo time.Time) (*models.Status, error) {
 	status, err := repo.FindByRouteId(ctx, RouteId)
 	if err != nil {
@@ -165,6 +242,80 @@ func (repo *MongoStatusRepo) FindByRouteIdAndUserIdBetween(ctx context.Context, 
 	}
 	if status.UserId != UserId {
 		return nil, &response.NotFound{Msg: fmt.Sprintf("route_id %v not found in the specified date range '%v' '%v' for the user '%v'", RouteId, DateFrom, DateTo, UserId)}
+	}
+	return status, nil
+}
+
+func (repo *MongoStatusRepo) FindByRouteIdAndVehicleIdBetween(ctx context.Context, RouteId string, VehicleId string, DateFrom time.Time, DateTo time.Time) (*models.Status, error) {
+	status, err := repo.FindByRouteId(ctx, RouteId)
+	if err != nil {
+		return nil, &response.NotFound{Msg: fmt.Sprintf("route_id %v not found", RouteId)}
+	}
+
+	routeStartTime := status.Timestamp[0].Time()
+
+	if !(DateBetween(routeStartTime, DateFrom, DateTo)) {
+		return nil, &response.NotFound{Msg: fmt.Sprintf("route_id %v not found in the specified date range '%v' '%v'", RouteId, DateFrom, DateTo)}
+	}
+	if status.VehicleId != VehicleId {
+		return nil, &response.NotFound{Msg: fmt.Sprintf("route_id %v not found in the specified date range '%v' '%v' for the vehicle '%v'", RouteId, DateFrom, DateTo, VehicleId)}
+	}
+	return status, nil
+}
+
+func (repo *MongoStatusRepo) FindByUserIdAndVehicleIdBetween(ctx context.Context, UserId string, VehicleId string, DateFrom time.Time, DateTo time.Time) ([]models.Status, error) {
+	cursor, err := repo.coordinatesCollection.Aggregate(ctx, mongo.Pipeline{
+		{{Key: "$match", Value: bson.D{
+			{Key: "meta.user_id", Value: UserId},
+			{Key: "ts", Value: bson.D{{Key: "$gt", Value: DateFrom}, {Key: "$lt", Value: DateTo}}},
+			{Key: "meta.vehicle_id", Value: VehicleId},
+		}}},
+		{{Key: "$group", Value: bson.D{
+			{Key: "_id", Value: "null"},
+			{Key: "ts", Value: bson.D{{Key: "$push", Value: "$ts"}}},
+			{Key: "coordinates", Value: bson.D{{Key: "$push", Value: "$location.coordinates"}}},
+			{Key: "speed", Value: bson.D{{Key: "$push", Value: bson.D{{Key: "$trunc", Value: bson.A{"$speed", 2}}}}}},
+			{Key: "route_id", Value: bson.D{{Key: "$first", Value: "$meta.route_id"}}},
+			{Key: "vehicle_id", Value: bson.D{{Key: "$first", Value: "$meta.vehicle_id"}}},
+			{Key: "user_id", Value: bson.D{{Key: "$first", Value: "$meta.user_id"}}},
+		}}},
+		{{Key: "$project", Value: bson.D{
+			{Key: "_id", Value: 0},
+		}}},
+	})
+	if err != nil {
+		return nil, &response.InternalError{Msg: "FindByUserIdAndVehicleIdBetween aggregation generated error:" + err.Error()}
+	}
+
+	defer cursor.Close(ctx)
+
+	var results []models.Status
+	if err = cursor.All(ctx, &results); err != nil {
+		return nil, &response.InternalError{Msg: "FindByUserIdAndVehicleIdBetween error unmarshalling bson response into Status model: " + err.Error()}
+	}
+
+	if len(results) == 0 {
+		return nil, &response.NotFound{Msg: fmt.Sprintf("No routes found for vehicle_id %v and user_id %v between the specified dates %v-%v", VehicleId, UserId, DateFrom, DateTo)}
+	}
+	return results, nil
+}
+
+func (repo *MongoStatusRepo) FindByRouteIdAndUserIdAndVehicleIdBetween(ctx context.Context, RouteId string, UserId string, VehicleId string, DateFrom time.Time, DateTo time.Time) (*models.Status, error) {
+	status, err := repo.FindByRouteId(ctx, RouteId)
+	if err != nil {
+		return nil, &response.NotFound{Msg: fmt.Sprintf("route_id %v not found", RouteId)}
+	}
+
+	routeStartTime := status.Timestamp[0].Time()
+
+	if !(DateBetween(routeStartTime, DateFrom, DateTo)) {
+		return nil, &response.NotFound{Msg: fmt.Sprintf("route_id %v not found in the specified date range '%v' '%v'", RouteId, DateFrom, DateTo)}
+	}
+	if status.VehicleId != VehicleId {
+		return nil, &response.NotFound{Msg: fmt.Sprintf("route_id %v not found in the specified date range '%v' '%v' for the vehicle '%v'", RouteId, DateFrom, DateTo, VehicleId)}
+	}
+	if status.UserId != UserId {
+		return nil, &response.NotFound{Msg: fmt.Sprintf("route_id %v not found in the specified date range '%v' '%v' for the vehicle '%v' and user %v", RouteId, DateFrom, DateTo, VehicleId, UserId)}
 	}
 	return status, nil
 }
--- vehicle-coordinates-api/services/status_service.go
+++ vehicle-coordinates-api/services/status_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -23,7 +24,7 @@ func NewStatusService(statusRepo db.StatusRepo) *StatusService {
 
 func (service *StatusService) GetRouteById(w http.ResponseWriter, r *http.Request, ctx context.Context) {
 
-	routeID, userID, dateFrom, dateTo, err := GetValidatedFilters(r)
+	routeID, userID, vehicleID, dateFrom, dateTo, err := GetValidatedFilters(r)
 	if err != nil {
 		response.HandleErrorResponseErr(w, err)
 		return
@@ -32,18 +33,37 @@ func (service *StatusService) GetRouteById(w http.ResponseWriter, r *http.Reques
 	var statusList []models.Status
 	var status *models.Status
 
-	if routeIDSet(routeID, userID, dateFrom, dateTo) {
+	if routeIDSet(routeID, userID, vehicleID, dateFrom, dateTo) {
 		status, err = service.statusRepo.FindByRouteId(ctx, routeID)
-	} else if userIDSet(routeID, userID, dateFrom, dateTo) {
-		statusList, err = service.statusRepo.FindByUserId(ctx, userID)
-	} else if routeIDAndDatesSet(routeID, userID, dateFrom, dateTo) {
+	} else if routeIDAndDatesSet(routeID, userID, vehicleID, dateFrom, dateTo) {
 		status, err = service.statusRepo.FindByRouteIdBetween(ctx, routeID, dateFrom, dateTo)
-	} else if userIDAndDatesSet(routeID, userID, dateFrom, dateTo) {
+	} else if userIDSet(routeID, userID, vehicleID, dateFrom, dateTo) {
+		statusList, err = service.statusRepo.FindByUserId(ctx, userID)
+	} else if userIDAndDatesSet(routeID, userID, vehicleID, dateFrom, dateTo) {
 		statusList, err = service.statusRepo.FindByUserIdBetween(ctx, userID, dateFrom, dateTo)
-	} else if routeIDAndUserIDSet(routeID, userID, dateFrom, dateTo) {
+	} else if vehicleIDSet(routeID, userID, vehicleID, dateFrom, dateTo) {
+		statusList, err = service.statusRepo.FindByVehicleId(ctx, vehicleID)
+	} else if vehicleIDAndDatesSet(routeID, userID, vehicleID, dateFrom, dateTo) {
+		statusList, err = service.statusRepo.FindByVehicleIdBetween(ctx, vehicleID, dateFrom, dateTo)
+	} else if routeIDAndUserIDSet(routeID, userID, vehicleID, dateFrom, dateTo) {
 		status, err = service.statusRepo.FindByRouteIdAndUserIdBetween(ctx, routeID, userID, time.Time{}, time.Now())
-	} else {
+	} else if routeIDAndVehicleIDSet(routeID, userID, vehicleID, dateFrom, dateTo) {
+		status, err = service.statusRepo.FindByRouteIdAndVehicleIdBetween(ctx, routeID, vehicleID, time.Time{}, time.Now())
+	} else if userIDAndVehicleIDSet(routeID, userID, vehicleID, dateFrom, dateTo) {
+		statusList, err = service.statusRepo.FindByUserIdAndVehicleIdBetween(ctx, userID, vehicleID, time.Time{}, time.Now())
+	} else if routeIDAndUserIDAndDatesSet(routeID, userID, vehicleID, dateFrom, dateTo) {
 		status, err = service.statusRepo.FindByRouteIdAndUserIdBetween(ctx, routeID, userID, dateFrom, dateTo)
+	} else if userIDAndVehicleIDAndDatesSet(routeID, userID, vehicleID, dateFrom, dateTo) {
+		statusList, err = service.statusRepo.FindByUserIdAndVehicleIdBetween(ctx, userID, vehicleID, dateFrom, dateTo)
+	} else if routeIDAndVehicleIDAndDatesSet(routeID, userID, vehicleID, dateFrom, dateTo) {
+		status, err = service.statusRepo.FindByRouteIdAndVehicleIdBetween(ctx, routeID, vehicleID, dateFrom, dateTo)
+	} else if routeIDAnduserIDAndVehicleIDSet(routeID, userID, vehicleID, dateFrom, dateTo) {
+		status, err = service.statusRepo.FindByRouteIdAndUserIdAndVehicleIdBetween(ctx, routeID, userID, vehicleID, time.Time{}, time.Now())
+	} else if allSet(routeID, userID, vehicleID, dateFrom, dateTo) {
+		status, err = service.statusRepo.FindByRouteIdAndUserIdAndVehicleIdBetween(ctx, routeID, userID, vehicleID, dateFrom, dateTo)
+	} else {
+		response.HandleErrorResponseErr(w, errors.New("invalid filters provided"))
+		return
 	}
 
 	if err != nil {
@@ -57,16 +77,17 @@ func (service *StatusService) GetRouteById(w http.ResponseWriter, r *http.Reques
 	response.HandleJsonResponse(w, http.StatusOK, statusList)
 }
 
-func GetValidatedFilters(r *http.Request) (string, string, time.Time, time.Time, error) {
+func GetValidatedFilters(r *http.Request) (string, string, string, time.Time, time.Time, error) {
 	errors := make([]string, 0)
 
 	var routeID string = r.URL.Query().Get("route_id")
 	var userID string = r.URL.Query().Get("user_id")
+	var vehicleID string = r.URL.Query().Get("vehicle_id")
 	var dateFromIn string = r.URL.Query().Get("date[gt]")
 	var dateToIn string = r.URL.Query().Get("date[lt]")
 
-	if routeID == "" && userID == "" && dateFromIn == "" && dateToIn == "" {
-		return "", "", time.Time{}, time.Time{}, &response.InvalidInput{Msg: "At least one of the following query parameters required: route_id, vehicle_id, date range (date[gt] and date[lt])"}
+	if routeID == "" && userID == "" && vehicleID == "" {
+		return "", "", "", time.Time{}, time.Time{}, &response.InvalidInput{Msg: "At least one of the following query parameters required: route_id, vehicle_id, user_id"}
 	}
 
 	var dateFrom time.Time
@@ -75,19 +96,19 @@ func GetValidatedFilters(r *http.Request) (string, string, time.Time, time.Time,
 	if dateFromIn != "" && dateToIn != "" {
 		dateFrom, dateTo, err = isDateRangeValid(&dateFromIn, &dateToIn, time.RFC3339)
 		if err != nil {
-			return "", "", time.Time{}, time.Time{}, &response.InvalidInput{Msg: err.Error()}
+			return "", "", "", time.Time{}, time.Time{}, &response.InvalidInput{Msg: err.Error()}
 		}
 	}
 	if (dateFromIn != "" && dateToIn == "") || (dateFromIn == "" && dateToIn != "") {
-		return "", "", time.Time{}, time.Time{}, &response.InvalidInput{Msg: "Invalid date range specified, date[gt] and date[lt] are required when specifying a date range"}
+		return "", "", "", time.Time{}, time.Time{}, &response.InvalidInput{Msg: "Invalid date range specified, date[gt] and date[lt] are required when specifying a date range"}
 	}
 
 	if len(errors) > 0 {
 		var errorMessage = fmt.Sprintf("Invalid query parameters provided: %s", strings.Join(errors, ","))
-		return "", "", time.Time{}, time.Time{}, &response.InvalidInput{Msg: errorMessage}
+		return "", "", "", time.Time{}, time.Time{}, &response.InvalidInput{Msg: errorMessage}
 	}
 
-	return routeID, userID, dateFrom, dateTo, nil
+	return routeID, userID, vehicleID, dateFrom, dateTo, nil
 }
 
 func isDateRangeValid(dateFromStr *string, dateToStr *string, format string) (time.Time, time.Time, error) {
@@ -112,28 +133,64 @@ func isDateRangeValid(dateFromStr *string, dateToStr *string, format string) (ti
 		}
 		errorStr = errorStr[:len(errorStr)-2]
 		return time.Time{}, time.Time{}, fmt.Errorf("invalid date range specified: %v", errorStr)
-	} else if dateFrom.After(dateTo) {
+	} else if dateFrom.After(dateTo) || dateFrom.Equal(dateTo) {
 		return time.Time{}, time.Time{}, fmt.Errorf("invalid date range specified: lower date bound must be lower than the higher date bound")
 	}
 	return dateFrom, dateTo, nil
 }
 
-func routeIDSet(routeID string, userID string, dateFrom time.Time, dateTo time.Time) bool {
-	return routeID != "" && userID == "" && dateFrom.IsZero() && dateTo.IsZero()
+func routeIDSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID != "" && userID == "" && vehicleID == "" && dateFrom.IsZero() && dateTo.IsZero()
 }
 
-func routeIDAndDatesSet(routeID string, userID string, dateFrom time.Time, dateTo time.Time) bool {
-	return routeID != "" && userID == "" && !dateFrom.IsZero() && !dateTo.IsZero()
+func routeIDAndDatesSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID != "" && userID == "" && vehicleID == "" && !dateFrom.IsZero() && !dateTo.IsZero()
 }
 
-func routeIDAndUserIDSet(routeID string, userID string, dateFrom time.Time, dateTo time.Time) bool {
-	return routeID != "" && userID != "" && dateFrom.IsZero() && dateTo.IsZero()
+func userIDSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID == "" && userID != "" && vehicleID == "" && dateFrom.IsZero() && dateTo.IsZero()
 }
 
-func userIDSet(routeID string, userID string, dateFrom time.Time, dateTo time.Time) bool {
-	return routeID == "" && userID != "" && dateFrom.IsZero() && dateTo.IsZero()
+func userIDAndDatesSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID == "" && userID != "" && vehicleID == "" && !dateFrom.IsZero() && !dateTo.IsZero()
 }
 
-func userIDAndDatesSet(routeID string, userID string, dateFrom time.Time, dateTo time.Time) bool {
-	return routeID == "" && userID != "" && !dateFrom.IsZero() && !dateTo.IsZero()
+func vehicleIDSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID == "" && userID == "" && vehicleID != "" && dateFrom.IsZero() && dateTo.IsZero()
+}
+
+func vehicleIDAndDatesSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID == "" && userID == "" && vehicleID != "" && !dateFrom.IsZero() && !dateTo.IsZero()
+}
+
+func routeIDAndUserIDSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID != "" && userID != "" && vehicleID == "" && dateFrom.IsZero() && dateTo.IsZero()
+}
+
+func routeIDAndVehicleIDSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID != "" && userID == "" && vehicleID != "" && dateFrom.IsZero() && dateTo.IsZero()
+}
+
+func userIDAndVehicleIDSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID == "" && userID != "" && vehicleID != "" && dateFrom.IsZero() && dateTo.IsZero()
+}
+
+func routeIDAndUserIDAndDatesSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID != "" && userID != "" && vehicleID == "" && !dateFrom.IsZero() && !dateTo.IsZero()
+}
+
+func routeIDAndVehicleIDAndDatesSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID != "" && userID == "" && vehicleID != "" && !dateFrom.IsZero() && !dateTo.IsZero()
+}
+
+func userIDAndVehicleIDAndDatesSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID == "" && userID != "" && vehicleID != "" && !dateFrom.IsZero() && !dateTo.IsZero()
+}
+
+func routeIDAnduserIDAndVehicleIDSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID != "" && userID != "" && vehicleID != "" && dateFrom.IsZero() && dateTo.IsZero()
+}
+
+func allSet(routeID string, userID string, vehicleID string, dateFrom time.Time, dateTo time.Time) bool {
+	return routeID != "" && userID != "" && vehicleID != "" && !dateFrom.IsZero() && !dateTo.IsZero()
 }
